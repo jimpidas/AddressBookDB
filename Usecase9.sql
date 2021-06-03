df2f57a21a1436e1bcc5696085de20b5c98b17d7
unknown <jimpidas>
2021-06-03 19:18:48 +0530

    [Jyosmita]Added: new addressbook name and type-family,friends,proffession

--- Usecase9.sql
+++ Usecase9.sql
@@ -1,4 +1,3 @@
-
 alter table AddressBookDB add Addressbookname varchar(20), type varchar(10)
 update AddressBookDB set Addressbookname='book1', type='Friends' where city='namrup'
 update AddressBookDB set Addressbookname='book2', type='Family' where city='tinsukia'
