20909b4e87498b14ed1776ddccd5f5b0f515727c
unknown <jimpidas>
2021-06-07 12:03:56 +0530

    [Jyosmita]Added: ability to adda person to both friends and family

---
+++ Usecase11.sql
@@ -0,0 +1,5 @@
+use Address_Book_ServiceDB
+
+insert into AddressBookDB values
+('Nikita','Sharma','House no 212','Tinsukia','Assam',785575,'8748892361','nikita@gmail.com','book2','Friends');
+select * from AddressBookDB
