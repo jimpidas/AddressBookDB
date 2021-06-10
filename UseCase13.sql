af866fcb352acd9a0de71f8d43e88c3979f2e440
unknown <jimpidas>
2021-06-10 19:13:09 +0530

    [Jyosmita]Added: With new table structure

--- UseCase13.sql
+++ UseCase13.sql
@@ -1,13 +1,9 @@
-use Address_Book_ServiceDB
-
-select p.firstname as "name",  a.city from person p inner join address a on p.person_id=a.person_id  where a.city='joypur';
+select p.firstname as "name",  a.city from person p inner join address a on p.zip=a.zip  where a.city='namrup';
 
 
-select a.city,count(city) as 'citycount' from Address a inner join person p on p.person_id=a.person_id group by city
-select a.state, count(state) as 'statecount' from Address a inner join person p on p.person_id=a.person_id group by state
+select a.city,count(city) as 'citycount' from Address a inner join person p on p.zip=a.zip group by city
+select a.state, count(state) as 'statecount' from Address a inner join person p on p.zip=a.zip group by state
 
+select p.firstname, a.city from person p inner join address a on p.zip=a.zip where city='namrup' order by (firstname)
 
-select p.firstname, a.city from person p inner join address a on p.person_id=a.person_id where city='namrup' order by (firstname)
-
-
-select b.book_type,count(book_type) as "typeCount" from AddressBook b inner join person p on b.person_id=p.person_id group by book_type
+select b.book_type,count(book_type) as "typeCount" from AddressBook b inner join person p on b.book_id=p.person_id group by book_type
